beb25047870cf4ab1ab44c4466633bcf6bebcf6d
Pascal7601 <levispascal63@gmail.com>
2024-08-21 05:44:00 -0700

    Caching base dict

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+file that sets up the base for
+caching
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    inherits from BaseCaching and implements
+    the put and get method
+    """
+    def put(self, key, item):
+        """
+        adds data in the cahed_data dict
+        """
+        if key or item is not None:
+            self.cache_data[key] = item
+
+
+    def get(self, key):
+        """
+        returns the value of the key
+        in the cached_data
+        """
+        if key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
