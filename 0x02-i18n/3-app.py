95316d6ce9291f072a339e6b238a437bb35aff3d
Pascal7601 <levispascal63@gmail.com>
2024-08-27 23:13:16 +0300

    handling translations in english and franch

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -3,23 +3,27 @@ from flask_babel import Babel
 
 
 app = Flask(__name__)
-babel = Babel(app)
+
 
 class Config:
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-    @babel.localeselector
-    def get_locale():
-        return request.accept_languages.best_match(app.config(['LANGUAGES']))
-
-
 app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    languages = app.config.get('LANGUAGES')
+    return request.accept_languages.best_match(languages)
+
+
 
 @app.get('/')
 def home():
-    return render_template('0-index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
